bc095e6a27159e83904785d0cbb4672760240514
Jitesh-Mahajan <Nimap@NIMAP>
2026-06-27 17:18:40 +0530

    Added BuildingBlocks

---
+++ src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace CatalogAPI.Products.CreateProduct
+{
+
+    //public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+    //: IRequest<CreateProductResult>;
+    //public record CreateProductResult(Guid Id);
+    //public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+    //{
+
+    //    Task<CreateProductResult> IRequestHandler<CreateProductCommand, CreateProductResult>.Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+    //}
+}
+
